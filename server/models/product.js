4c4c5889b4f6abb7fb9940fa6162e8a899f97f54
sergey <skhvan1111@gmail.com>
2016-07-09 21:39:15 +0500

    product access

--- server/models/product.js
+++ server/models/product.js
@@ -1,3 +1,12 @@
 module.exports = function(Product) {
 
+	var methods = [
+		{name:"createChangeStream", isStatic: true},
+		{name:"updateAll", isStatic: true},
+		{name:"updateAttributes", isStatic: false},
+	];
+
+	methods.forEach(function(method){
+		Product.disableRemoteMethod(method.name, method.isStatic);		
+	});
 };
